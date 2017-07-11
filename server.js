9a2b63e8263a184e7245959aa1bdf5f94fb97214
Víctor Fernández Rico <vfrico@gmail.com>
2017-07-11 11:01:10 +0200

    *Dockerizando* la aplicacion web

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ new WebpackDevServer(webpack(config), {
       chunks: false,
       chunkModules: false
     }
-}).listen(3000, 'localhost', function (err) {
+}).listen(3000, '0.0.0.0', function (err) {
     if (err) {
         console.log(err);
     }
